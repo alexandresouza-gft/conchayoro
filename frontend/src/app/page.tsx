402e32996f58e4a02c1160ecfced278d86fdf1f9
Alexandre Dias de Souza <aeia@gft.com>
2025-04-10 20:17:47 -0300

    Create FrontEnd

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full font-mono text-sm lg:flex items-center justify-between">
+        <h1 className="text-2xl font-bold">Welcome to the ConchaYOro App</h1>
+      </div>
+    </main>
+  );
+}
