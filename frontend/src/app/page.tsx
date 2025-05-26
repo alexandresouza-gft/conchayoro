6649ec290e52d6a5214ca99bed605c59d346c3c1
aeia@gft.com <aeia@gft.com>
2025-05-26 20:40:53 -0300

    Update

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full font-mono text-sm lg:flex items-center justify-between">
-        <h1 className="text-2xl font-bold">Welcome to the ConchaYOro App</h1>
+        <h1 className="text-2xl font-bold">Welcome to the ConchaYOro App - Deploy V2</h1>
       </div>
     </main>
   );
